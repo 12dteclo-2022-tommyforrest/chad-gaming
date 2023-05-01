3bc86858fbfef2af3169871ce3a26a56e830bf8d
Tommy Forrest <57433227+tommyforrest@users.noreply.github.com>
2023-05-01 12:56:02 +1200

    chore: set up testing and lsp integration for monorepo

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -2,8 +2,9 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 
 export default {
 	webServer: {
-		command: "npm run build && npm run preview",
+		command: "pnpm run build && pnpm run preview",
 		port: 4173,
 	},
 	testDir: "tests",
+	testMatch: /.*(test|spec)\.(js|ts|mjs)/,
 } satisfies PlaywrightTestConfig;
