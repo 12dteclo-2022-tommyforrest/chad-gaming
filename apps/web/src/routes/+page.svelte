aa188b7c688517e393e2427589de2418e17c4432
Tommy Forrest <57433227+tommyforrest@users.noreply.github.com>
2023-05-02 15:36:10 +1200

    feat: prototype page

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script lang="ts">
+	import Nav from "$lib/components/Nav.svelte";
+	import NavItem from "$lib/components/NavItem.svelte";
+
+	import logo from "$lib/assets/logo.png";
+</script>
+
+<header class="flex bg-slate-400 p-4">
+	<img class="flex-none w-8 h-fit inline-block" src={logo} alt="Brand logo." />
+	<Nav>
+		<NavItem name="Home" to="/" />
+		<NavItem name="Other Home" to="/" />
+	</Nav>
+</header>
+
+<h1 class="text-6xl text-center">Brand Name</h1>
+<h2 class="text-4xl text-center text-stone-700">Play games online</h2>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
