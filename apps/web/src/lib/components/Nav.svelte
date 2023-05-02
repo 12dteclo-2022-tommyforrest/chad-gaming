54ed4c77b4b36001c9fc1421221f8eb504074245
Tommy Forrest <57433227+tommyforrest@users.noreply.github.com>
2023-05-03 11:00:01 +1200

    feat(nav): add shadow to hamburger menu, make logo size more consistent on medium screens

--- apps/web/src/lib/components/Nav.svelte
+++ apps/web/src/lib/components/Nav.svelte
@@ -11,34 +11,45 @@
 </script>
 
 <nav class="grid grid-cols-11 md:grid-cols-20 row-auto">
-	<a class="col-span-1" href="/">
-		<img class="w-8 h-fit" src={logo} alt="Brand logo." />
-	</a>
+	<!-- Container for shadow on hamburger menu, switches to flexbox to center logo on medium screens -->
+	<div
+		class={`col-span-full p-4 md:p-0 md:pl-4 md:col-span-1 grid grid-cols-11 md:flex md:justify-center md:items-center ${
+			open && "shadow-sm shadow-black md:shadow-none"
+		}`}
+	>
+		<!-- Logo - links back to home page -->
+		<a class="col-span-1" href="/">
+			<img class="w-8 h-fit" src={logo} alt="Chad Gaming logo." />
+		</a>
 
-	<div class="col-span-9 md:hidden" />
-
-	<div class="block md:hidden" data-testid="hamburger">
-		<div class="cursor-pointer mt-0.5 md:hidden" on:click={toggleOpen} on:keypress={toggleOpen}>
+		<!-- Hamburger menu, manually created with 3 divs and an overarching container that handles interactivity -->
+		<div class="block col-start-12 md:hidden" data-testid="hamburger">
 			<div
-				class="w-9 h-1 bg-slate-950 mb-2 transition-all duration-300"
-				class:barOneOpen={open}
-			/>
-			<div
-				class={`w-9 h-1 bg-slate-950 mb-2 transition-all duration-300 ${
-					open && barTwoOpen
-				}`}
-			/>
-			<div
-				class="w-9 h-1 bg-slate-950 transition-all duration-300"
-				class:barThreeOpen={open}
-			/>
+				class="cursor-pointer mt-0.5 md:hidden"
+				on:click={toggleOpen}
+				on:keypress={toggleOpen}
+			>
+				<div
+					class="w-9 h-1 bg-slate-950 mb-2 transition-all duration-300"
+					class:barOneOpen={open}
+				/>
+				<div
+					class={`w-9 h-1 bg-slate-950 mb-2 transition-all duration-300 ${
+						open && barTwoOpen
+					}`}
+				/>
+				<div
+					class="w-9 h-1 bg-slate-950 transition-all duration-300"
+					class:barThreeOpen={open}
+				/>
+			</div>
 		</div>
 	</div>
 
 	<ul
 		class={`${
 			open ? "block" : "hidden"
-		} col-span-full list-none align-middle md:block md:col-[span_19]`}
+		} col-span-full list-none p-4 align-middle md:block md:col-[span_19]`}
 	>
 		<slot />
 	</ul>
