2005e6fb367e06beb82dc9e49695c061f829d9e4
Tommy Forrest <57433227+tommyforrest@users.noreply.github.com>
2023-05-02 15:35:03 +1200

    feat(nav): add `Nav` ^& `NavItem`

---
+++ apps/web/src/lib/components/Nav.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	let open = false;
+
+	const barTwoOpen = "opacity-0";
+
+	const toggleOpen = () => {
+		open = !open;
+	};
+</script>
+
+<nav class="grow inline align-middle">
+	<ul class="list-none hidden align-middle md:block">
+		<slot />
+	</ul>
+
+	<div
+		class="cursor-pointer mt-0.5 float-right md:hidden"
+		on:click={toggleOpen}
+		on:keypress={toggleOpen}
+	>
+		<div
+			class="w-9 h-1 bg-slate-950 mb-2 transition-all duration-300"
+			class:barOneOpen={open}
+		/>
+		<div
+			class={`w-9 h-1 bg-slate-950 mb-2 transition-all duration-300 ${open && barTwoOpen}`}
+		/>
+		<div class="w-9 h-1 bg-slate-950 transition-all duration-300" class:barThreeOpen={open} />
+	</div>
+</nav>
+
+<style lang="postcss">
+	.barOneOpen {
+		transform: rotate(45deg) translate(8px, 11px);
+	}
+
+	.barThreeOpen {
+		transform: rotate(-45deg) translate(5px, -10px);
+	}
+</style>
---
+++ apps/web/src/lib/components/NavItem.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let name: string;
+	export let to: string;
+</script>
+
+<li class="inline p-2">
+	<a href={to}>{name}</a>
+</li>
