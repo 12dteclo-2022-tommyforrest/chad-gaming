f32ee9fca6dc6e3f62ee42d90a5691d733144f67
Tommy Forrest <57433227+tommyforrest@users.noreply.github.com>
2023-05-03 10:58:56 +1200

    feat: add brand name and adjust styling

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -5,15 +5,15 @@
 	import logo from "$lib/assets/logo.png";
 </script>
 
-<header class="bg-slate-400 p-4">
+<header class="bg-slate-400">
 	<Nav>
 		<NavItem name="Home" to="/" />
 		<NavItem name="Other Home" to="/" />
 	</Nav>
 </header>
 
-<h1 class="text-6xl text-center">Brand Name</h1>
-<h2 class="text-4xl text-center text-stone-700">Play games online</h2>
+<h1 class="text-5xl text-center">Chad Gaming</h1>
+<h2 class="text-3xl text-center text-stone-700">Play games online</h2>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
