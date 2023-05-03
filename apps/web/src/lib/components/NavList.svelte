aa17e6b1efbf47ec8ae3a3f5f9327ea09279aba1
Tommy Forrest <57433227+tommyforrest@users.noreply.github.com>
2023-05-03 12:15:16 +1200

    feat: create `NavList` component

---
+++ apps/web/src/lib/components/NavList.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { slide } from "svelte/transition";
+
+	export let open: boolean;
+	export let transition = false;
+</script>
+
+{#if transition}
+	<ul
+		class={`${
+			open ? "block" : "hidden"
+		} col-span-full list-none p-4 align-middle md:block md:col-[span_19] shadow-sm shadow-slate-700 md:shadow-none`}
+		transition:slide={{ duration: 250 }}
+	>
+		<slot />
+	</ul>
+{:else}
+	<ul
+		class={`${
+			open ? "block" : "hidden"
+		} col-span-full list-none p-4 align-middle md:block md:col-[span_19] shadow-sm shadow-slate-700 md:shadow-none`}
+	>
+		<slot />
+	</ul>
+{/if}
