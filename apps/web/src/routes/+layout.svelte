d2a194043080b8bd2ccbdbf62c0ea2bf2ad1fc5f
Tommy Forrest <57433227+tommyforrest@users.noreply.github.com>
2023-05-03 12:22:17 +1200

    feat: move Nav from page to layout

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
+	import Nav from "$lib/components/Nav.svelte";
+	import NavItem from "$lib/components/NavItem.svelte";
 	import "../app.css";
 </script>
+
+<header class="bg-white">
+	<Nav>
+		<NavItem name="Home" to="/" />
+		<NavItem name="Other Home" to="/" />
+	</Nav>
+</header>
 
 <slot />
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -5,12 +5,5 @@
 	import logo from "$lib/assets/logo.png";
 </script>
 
-<header class="bg-white">
-	<Nav>
-		<NavItem name="Home" to="/" />
-		<NavItem name="Other Home" to="/" />
-	</Nav>
-</header>
-
 <h1 class="text-5xl text-center">Chad Gaming</h1>
 <h2 class="text-3xl text-center text-stone-700">Play games online</h2>
