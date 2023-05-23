adec1a9380dcac8a9ba6de74db66083931f0f1f5
Tommy Forrest <57433227+tommyforrest@users.noreply.github.com>
2023-05-24 09:33:47 +1200

    feat(web/footer): create footer

---
+++ apps/web/src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	const currentYear = new Date().getFullYear();
+</script>
+
+<footer class="w-full border-t-2 border-black p-4 text-center">
+	<p>&copy; {currentYear} Tommy Forrest</p>
+</footer>
