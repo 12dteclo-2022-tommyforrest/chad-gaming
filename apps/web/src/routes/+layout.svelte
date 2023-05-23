1516205cc961230060e70d36a88e7725f20d0fb8
Tommy Forrest <57433227+tommyforrest@users.noreply.github.com>
2023-05-24 09:33:33 +1200

    feat(web/layout): format footer to always remain at the bottom

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,8 +1,20 @@
 <script>
+	import Footer from "$lib/components/Footer.svelte";
 	import Header from "$lib/components/Header.svelte";
 	import "../app.css";
 </script>
 
 <Header />
+<main>
+	<slot />
+</main>
+<Footer />
 
-<slot />
+<style lang="postcss">
+	/* Keeps footer on the bottom of the page */
+	:global(body) {
+		min-height: 100vh;
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+	}
+</style>
