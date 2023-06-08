fd145255b2be3eb568d52c578b10f68d8659d424
Tommy Forrest <57433227+tommyforrest@users.noreply.github.com>
2023-06-08 12:26:03 +1200

    feat(web): add `GameCard` and `GameList`

---
+++ apps/web/src/lib/components/GameCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let img: string;
+	export let title: string;
+	export let description: string;
+
+	const imageData = import(img);
+</script>
+
+<div>
+	{#await imageData then data}
+		<img src={data} alt={title} />
+	{/await}
+	<h3>{title}</h3>
+	<p>{description}</p>
+</div>
