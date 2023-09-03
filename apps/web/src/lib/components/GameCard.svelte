74fffa3d31248c11c46415233dd4e8f4fd52284b
Tommy Forrest <57433227+tommyforrest@users.noreply.github.com>
2023-09-03 15:58:49 +1200

    fix(web/gamecard): temporarily use Tic Tac Toe image while previews are bugged

--- apps/web/src/lib/components/GameCard.svelte
+++ apps/web/src/lib/components/GameCard.svelte
@@ -3,13 +3,15 @@
 	export let title: string;
 	export let description: string;
 	export let url: string;
+
+	import tempimg from "$lib/assets/tic-tac-toe.png";
 </script>
 
 <a href={url}>
 	<div
 		class="rounded-xl bg-zinc-400 p-4 shadow-md shadow-black hover:shadow-lg hover:shadow-black hover:transition-shadow"
 	>
-		<img class="mx-auto aspect-square h-36 w-fit" src={img} alt={title} />
+		<img class="mx-auto aspect-square h-36 w-fit" src={tempimg} alt={title} />
 		<h3 class="text-center text-xl font-semibold">{title}</h3>
 		<p class="text-center text-lg">{description}</p>
 	</div>
