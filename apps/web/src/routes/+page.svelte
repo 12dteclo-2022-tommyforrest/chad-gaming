b41e2e9943866ae98ac23824141e601cb08374e5
Tommy Forrest <57433227+tommyforrest@users.noreply.github.com>
2023-09-29 14:43:50 +1300

    feat(web/home): add GameList to home page

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+	import GameList from "$lib/components/GameList.svelte";
+</script>
+
 <h1 class="text-center text-5xl">Chad Gaming</h1>
 <h2 class="text-center text-3xl text-stone-700">Play games online</h2>
+
+<br />
+
+<h2 class="text-center text-3xl">Our Games</h2>
+<GameList />
